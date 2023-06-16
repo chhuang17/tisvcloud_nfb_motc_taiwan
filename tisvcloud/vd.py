1ffa8cfaac4675f87fa2dde212ddce2f0119974a
Chien-Hao Huang <81426493+chhuang17@users.noreply.github.com>
2023-06-16 15:49:52 +0800

    Add files via upload

--- tisvcloud/vd.py
+++ tisvcloud/vd.py
@@ -4,7 +4,17 @@ import gzip
 import numpy as np
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+
+""" Package update on 2023-06-02
+    
+    In version 0.1.1, we have fixed the bug of ZeroDivisionError.
+    Also, we have added some new functions:
+    1. In version 0.1.1, you can get multiple VD Data (including volume, speed, and occupancy) at once.
+    2. In version 0.1.1, you can use the multiple_volume() to get the volume for each vehicle type
+       by setting the parameter "vehType".
+"""
+
 
 # Hierachy 0
 # VDLives:           root[-1]
@@ -51,8 +61,8 @@ def download(date, hour, minute):
     if vdlive.empty():
         return f"Cannot find VDLiveData: {vdlive.date}/VDLive_{vdlive.hour}{vdlive.minute}.xml"
 
-def volume(VDID, date, hour, minute):
-    volume = 0
+def volume(VDID, date, hour, minute, vehType="all"):
+    volume, vol_S, vol_L, vol_T = 0, 0, 0, 0
     vdlive = MOTCLiveData(date, hour, minute, dataname="VD")
     vdlive.download()
     if vdlive.empty():
@@ -73,11 +83,25 @@ def volume(VDID, date, hour, minute):
                         Lane = VDLives[i][1][0][1]
                         for j in range(len(Lane)):
                             Vehicle = Lane[j][-1]
-                            for k in range(len(Vehicle)):
-                                volume += int(Vehicle[k][1].text)
+                            if vehType == "all":
+                                for k in range(len(Vehicle)):
+                                    volume += int(Vehicle[k][1].text)
+                            elif vehType == "each":
+                                vol_S += int(Vehicle[0][1].text)
+                                vol_L += int(Vehicle[1][1].text)
+                                vol_T += int(Vehicle[2][1].text)
+                            elif vehType == "S":
+                                volume += int(Vehicle[0][1].text)
+                            elif vehType == "L":
+                                volume += int(Vehicle[1][1].text)
+                            elif vehType == "T":
+                                volume += int(Vehicle[2][1].text)
                         xmlfile.close()
                         vdlive.delete()
-                        return volume
+                        if vehType == "each":
+                            return (vol_S, vol_L, vol_T)
+                        else:
+                            return volume
                     
                     elif VDLives[i][2].text == "1":
                         xmlfile.close()
@@ -122,7 +146,11 @@ def speed(VDID, date, hour, minute):
                                 volumeXspeed += int(Vehicle[k][1].text) * int(Vehicle[k][2].text)
                         xmlfile.close()
                         vdlive.delete()
-                        return volumeXspeed / volume
+                        try:
+                            speed = volumeXspeed / volume
+                        except ZeroDivisionError:
+                            speed = freeFlowSpeed
+                        return speed
                     
                     elif VDLives[i][2].text == "1":
                         xmlfile.close()
@@ -180,3 +208,172 @@ def occupy(VDID, date, hour, minute):
                         vdlive.delete()
                         return f"{VDID} 設備故障!"
     
+def multiple_volume(VDList, date, hour, minute, vehType="all", errorCode=-1):
+    vdlive = MOTCLiveData(date, hour, minute, dataname="VD")
+    vdlive.download()
+    if vdlive.empty():
+        print(f"Cannot find VDLiveData: {vdlive.date}/VDLive_{vdlive.hour}{vdlive.minute}.xml")
+        return [errorCode for i in range(len(VDList))]
+    
+    data = []
+    for VDID in VDList:
+        volume, vol_S, vol_L, vol_T = 0, 0, 0, 0
+        with gzip.open(vdlive.filename, "r") as xmlfile:
+            try:
+                tree = ET.parse(xmlfile)
+                root = tree.getroot()
+                VDLives = root[-1]
+            
+            except ET.ParseError:
+                print(f"{vdlive.filename} is Empty!")
+            
+            else:
+                for i in range(len(VDLives)):
+                    if VDID == VDLives[i][0].text:
+                        if VDLives[i][2].text == "0":
+                            Lane = VDLives[i][1][0][1]
+                            for j in range(len(Lane)):
+                                Vehicle = Lane[j][-1]
+                                if vehType == "all":
+                                    for k in range(len(Vehicle)):
+                                        volume += int(Vehicle[k][1].text)
+                                elif vehType == "each":
+                                    vol_S += int(Vehicle[0][1].text)
+                                    vol_L += int(Vehicle[1][1].text)
+                                    vol_T += int(Vehicle[2][1].text)
+                                elif vehType == "S":
+                                    volume += int(Vehicle[0][1].text)
+                                elif vehType == "L":
+                                    volume += int(Vehicle[1][1].text)
+                                elif vehType == "T":
+                                    volume += int(Vehicle[2][1].text)
+                            xmlfile.close()
+                            if vehType == "each":
+                                data.append((vol_S, vol_L, vol_T))
+                            else:
+                                data.append(volume)
+                            continue
+                        
+                        elif VDLives[i][2].text == "1":
+                            xmlfile.close()
+                            if vehType == "each":
+                                data.append((-1, -1, -1))
+                            else:
+                                data.append(errorCode)
+                            continue
+                        
+                        elif VDLives[i][2].text == "2":
+                            xmlfile.close()
+                            if vehType == "each":
+                                data.append((-1, -1, -1))
+                            else:
+                                data.append(errorCode)
+                            continue
+                        
+                        elif VDLives[i][2].text == "3":
+                            xmlfile.close()
+                            if vehType == "each":
+                                data.append((-1, -1, -1))
+                            else:
+                                data.append(errorCode)
+                            continue
+    return data
+    
+def multiple_speed(VDList, date, hour, minute, freeFlowSpeed=100, errorCode=-1):
+    vdlive = MOTCLiveData(date, hour, minute, dataname="VD")
+    vdlive.download()
+    if vdlive.empty():
+        print(f"Cannot find VDLiveData: {vdlive.date}/VDLive_{vdlive.hour}{vdlive.minute}.xml")
+        return [errorCode for i in range(len(VDList))]
+    
+    data = []
+    for VDID in VDList:
+        volume = 0
+        volumeXspeed = 0
+        with gzip.open(vdlive.filename, "r") as xmlfile:
+            try:
+                tree = ET.parse(xmlfile)
+                root = tree.getroot()
+                VDLives = root[-1]
+            
+            except ET.ParseError:
+                print(f"{vdlive.filename} is Empty!")
+            
+            else:
+                for i in range(len(VDLives)):
+                    if VDID == VDLives[i][0].text:
+                        if VDLives[i][2].text == "0":
+                            Lane = VDLives[i][1][0][1]
+                            for j in range(len(Lane)):
+                                Vehicle = Lane[j][-1]
+                                for k in range(len(Vehicle)):
+                                    volume += int(Vehicle[k][1].text)
+                                    volumeXspeed += int(Vehicle[k][1].text) * int(Vehicle[k][2].text)
+                            xmlfile.close()
+                            try:
+                                data.append(volumeXspeed / volume)
+                            except ZeroDivisionError:
+                                data.append(freeFlowSpeed)
+                            continue
+                        
+                        elif VDLives[i][2].text == "1":
+                            xmlfile.close()
+                            data.append(errorCode)
+                            continue
+                        
+                        elif VDLives[i][2].text == "2":
+                            xmlfile.close()
+                            data.append(errorCode)
+                            continue
+                        
+                        elif VDLives[i][2].text == "3":
+                            xmlfile.close()
+                            data.append(errorCode)
+                            continue
+    return data
+    
+def multiple_occupy(VDList, date, hour, minute, errorCode=-1):
+    vdlive = MOTCLiveData(date, hour, minute, dataname="VD")
+    vdlive.download()
+    if vdlive.empty():
+        print(f"Cannot find VDLiveData: {vdlive.date}/VDLive_{vdlive.hour}{vdlive.minute}.xml")
+        return [errorCode for i in range(len(VDList))]
+    
+    data = []
+    for VDID in VDList:
+        occupy = 0
+        with gzip.open(vdlive.filename, "r") as xmlfile:
+            try:
+                tree = ET.parse(xmlfile)
+                root = tree.getroot()
+                VDLives = root[-1]
+            
+            except ET.ParseError:
+                print(f"{vdlive.filename} is Empty!")
+            
+            else:
+                for i in range(len(VDLives)):
+                    if VDID == VDLives[i][0].text:
+                        if VDLives[i][2].text == "0":
+                            Lane = VDLives[i][1][0][1]
+                            for j in range(len(Lane)):
+                                occupy += int(Lane[j][3].text)
+                            xmlfile.close()
+                            data.append(occupy / len(Lane))
+                            continue
+                        
+                        elif VDLives[i][2].text == "1":
+                            xmlfile.close()
+                            data.append(errorCode)
+                            continue
+                        
+                        elif VDLives[i][2].text == "2":
+                            xmlfile.close()
+                            data.append(errorCode)
+                            continue
+                        
+                        elif VDLives[i][2].text == "3":
+                            xmlfile.close()
+                            data.append(errorCode)
+                            continue
+    return data
